f9f44f5f949d9798c545912548ef1b5c406f5be1
Lucas dos Anjos <anjoslucas55@gmail.com>
2023-01-26 21:38:02 -0300

    Criar a lógica de contador de segundos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-const segundosPomodoro = 4000 //25*60*1000
+const segundosPomodoro = 4000 //Estamos usando 4 segundos. O tempo oficial de 25*60*1000
 const disparador = document.querySelector('#disparador')
 
 disparador.addEventListener('click', () => {
     console.log("Disparador ativado.")
+
+    // Criando o cronometro
     setTimeout( () => {
         console.log("Acabou o pomodoro!");
-        alert("O seu tempo de produção de pomodoro acabou. Vá descansar!")
-    }, 4000 )
+        alert("O seu tempo de produção de pomodoro acabou. Vá descansar!") 
+        clearInterval(contador)  
+    }, segundosPomodoro)
+    
+    let contador = setInterval('contadorDeSegundos()',1000);
 })
+
+function contadorDeSegundos() {
+    console.log('tick')
+}
 
